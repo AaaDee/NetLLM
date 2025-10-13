b76c704334aa9db3a3e73706c00ed1fef52a4c1b
AaaDee <aarne.rissanen@gmail.com>
2025-10-13 20:05:30 +0200

    update model training

--- adaptive_bitrate_streaming/run_trained_model.sh
+++ adaptive_bitrate_streaming/run_trained_model.sh
@@ -1,5 +1,13 @@
-### Runs the adaptive bitrate streaming test code with a llm model checkpoint
-srun --interactive -c4 --mem=32G -G1 -t04:00:00 -pgpu-oversub -M ukko --pty bash
+#!/bin/bash
+#SBATCH --job-name=abr-testing
+#SBATCH -o abr-test-%J.txt
+#SBATCH -M kale
+#SBATCH -p gpu
+#SBATCH --cpus-per-gpu=2
+#SBATCH -G1
+#SBATCH --mem-per-cpu=8000
+#SBATCH --time=48:00:00
+
 cd /turso/wrk-vakka/users/aarneris/NetLLM
 
 ### Install Anaconda to manage conda environments (if necessary)
@@ -13,7 +21,7 @@ bash Miniconda3-latest-Linux-x86_64.sh -u -b
 
 cd /turso/wrk-vakka/users/aarneris/NetLLM/NetLLM/adaptive_bitrate_streaming
 conda env create -f environment.yaml
-conda activate vp_netllm
+conda activate abr_netllm
 
 pip install -r requirements.txt
 # Has to be installed separately due to a dependency conflict
@@ -22,5 +30,5 @@ pip install huggingface-hub==0.23.0 --no-dependencies
 python -m pip install torch==1.10.2
 # Test command as presented in the NetLLM repository
 
-# Run with the best model checkpoint
+# Run with the best model checkpoint (from training)
 python run_plm.py --test --plm-type llama --plm-size base --rank 128 --device cuda:0 --model-dir  data/ft_plms/llama_base/artifacts_exp_pools_ss_None/rank_128_w_20_gamma_1.0_sfd_256_lr_0.0001_wd_0.0001_warm_2000_epochs_80_seed_100003/early_stop_-1_best_model
