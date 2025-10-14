207c7199a89c39e66a135d64ccf0ad6bbf8185bf
AaaDee <aarne.rissanen@gmail.com>
2025-10-14 22:24:22 +0200

    update run_training_lm4

--- viewport_prediction/run_training_lm4.sh
+++ viewport_prediction/run_training_lm4.sh
@@ -32,12 +32,6 @@ pip install huggingface-hub==0.23.0 --no-dependencies
 pip install transformers==4.57.0
 
 # Run the training from start
-python run_plm.py --adapt --train-dataset Jin2022 --his-window 10 --fut-window 20 --plm-type llama --plm-size base --epochs 40 --bs 1 --lr 0.0002 --grad-accum-steps 32 --device cuda:0 --steps-per-valid 5000 --save-checkpoint-per-epoch 1 --rank 32 --scheduled-sampling --plm-path ../../llama4/Llama-4-Scout-17B-16E
+python run_plm.py --adapt --train-dataset Jin2022 --his-window 10 --fut-window 20 --plm-type llama4 --plm-size base --epochs 40 --bs 1 --lr 0.0002 --grad-accum-steps 32 --device cuda:0 --steps-per-valid 5000 --save-checkpoint-per-epoch 1 --rank 32 --scheduled-sampling --plm-path ../../llama4/Llama-4-Scout-17B-16E
  
 
-
-# Run the training from a checkpoint
-# python run_plm.py --adapt --train-dataset Jin2022 --his-window 10 --fut-window 20 --plm-type llama --plm-size base --epochs 40 --bs 1 --lr 0.0002 --grad-accum-steps 32 --device cuda:0 --steps-per-valid 5000 --save-checkpoint-per-epoch 1 --rank 32 --scheduled-sampling --resume --resume-path data/ft_plms/llama_base_low_rank/freeze_plm_False/Jin2022/5Hz/his_10_fut_20_ss_15_epochs_40_bs_32_lr_0.0002_seed_1_rank_32_scheduled_sampling_True/checkpoint
-
-# Run interactive
-# srun --interactive --cpus-per-gpu=2 --mem-per-cpu=8000 -G1 -t08:00:00 -pgpu -M ukko --pty bash
