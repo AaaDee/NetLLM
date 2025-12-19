2984d36c7227a4cb4990b898b11ddb959fd359b7
AaaDee <aarne.rissanen@gmail.com>
2025-12-19 11:31:56 +0100

    update environement

--- cluster_job_scheduling/run_training_lm3.sh
+++ cluster_job_scheduling/run_training_lm3.sh
@@ -31,21 +31,21 @@ conda install -c conda-forge gxx -y
 
 # For Gymnasium
 conda install swig -y
-pip install "Gymnasium[all]"
+# pip install "Gymnasium[all]"
 
-pip install torch==2.8.0 --index-url https://download.pytorch.org/whl/cu129
+# pip install torch==2.8.0 --index-url https://download.pytorch.org/whl/cu129
 
-pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-2.8.0+cu129
-pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-2.8.0+cu129
-pip install torch-cluster -f https://pytorch-geometric.com/whl/torch-2.8.0+cu129
-pip install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-2.8.0+cu129
-pip install torch-geometric -i https://pypi.tuna.tsinghua.edu.cn/simple_torch_geometric
+# pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-2.8.0+cu129
+# pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-2.8.0+cu129
+# pip install torch-cluster -f https://pytorch-geometric.com/whl/torch-2.8.0+cu129
+# pip install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-2.8.0+cu129
+# pip install torch-geometric -i https://pypi.tuna.tsinghua.edu.cn/simple_torch_geometric
 
-pip install numpy==2.1.0
-pip install munch==4.0.0
-pip install openprompt==1.0.1
-pip install peft==0.13.2
-pip install torch_geometric==2.7.0
+# pip install numpy==2.1.0
+# pip install munch==4.0.0
+# pip install openprompt==1.0.1
+# pip install peft==0.13.2
+# pip install torch_geometric==2.7.0
 
 ##################################
 
