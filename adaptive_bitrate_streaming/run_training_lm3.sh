52328376d25dc6aa83da8d14669e26ebb4e894d5
AarneRissanen-Freiheit <aarne.rissanen@freiheit.com>
2025-12-30 18:39:02 +0100

    abr llm3 updates

--- adaptive_bitrate_streaming/run_training_lm3.sh
+++ adaptive_bitrate_streaming/run_training_lm3.sh
@@ -1,36 +1,8 @@
-#!/bin/bash
-#SBATCH --job-name=abr-training-lm3
-#SBATCH -o abr-training-lm3-%J.txt
-#SBATCH -M ukko
-#SBATCH -p gpu
-#SBATCH --cpus-per-gpu=2
-#SBATCH -G1
-#SBATCH --mem-per-cpu=8000
-#SBATCH --time=48:00:00
+# Runs the abr training for llama3
+# To be run on a CUDA docker image (or similar) with most of the tools preinstalled
 
-
-### Finetunes the adaptive bitrate streaming model
-cd /turso/wrk-vakka/users/aarneris/NetLLM
-
-### Install Anaconda to manage conda environments (if necessary)
-# Needed as conda uses the home path for some operations
-export HOME=/turso/wrk-vakka/users/aarneris/NetLLM
-cd miniconda3
-# download miniconda installer if not loaded yet
-# wget https://repo.continuum.io/archive/Anaconda3-5.1.0-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh -u -b
-
-# Activate conda if installed
-. bin/activate
-
-cd /turso/wrk-vakka/users/aarneris/NetLLM/NetLLM/adaptive_bitrate_streaming
 conda env create -f environment_3.yaml
 conda activate abr_netllm_3
 
 # Run the training from start
 python run_plm.py --adapt --grad-accum-steps 32 --plm-type llama --plm-size base --rank 128 --device cuda:0 --lr 0.0001 --warmup-steps 2000 --num-epochs 80 --eval-per-epoch 2 --plm-path ../../Llama-3.1-8B
-
- 
-
-# Run interactive
-# srun --interactive --cpus-per-gpu=2 --mem-per-cpu=8000 -G1 -t08:00:00 -p gpu -M kale --pty bash
