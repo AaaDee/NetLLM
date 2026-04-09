8f9c79763779107035310f63c91eaa0291037379
Aarne Rissanen <aarne.rissanen@gmail.com>
2026-04-09 19:36:33 +0300

    add thousands separator to y-axis

--- result_calculations/graphs/mentions_graph.py
+++ result_calculations/graphs/mentions_graph.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import pandas as pd
 
 
@@ -17,9 +18,13 @@ plt.bar(df['Year'], df['Mentions'], color=color, edgecolor='black')
 plt.xlabel('Year')
 plt.ylabel("Number of Mentions ('LLM')")
 
+# Thousands separator
+plt.gca().yaxis.set_major_formatter(ticker.StrMethodFormatter('{x:,.0f}'))
+
 for i, v in enumerate(df['Mentions']):
     plt.text(df['Year'][i], v + 1000, f'{v:,}', ha='center', fontweight='bold')
 
+plt.grid(axis='y', linestyle='--', alpha=0.7)
 
 plt.tight_layout()
 plt.savefig('llm_mentions_chart.pdf')
